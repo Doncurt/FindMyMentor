a5b555b6dc009a6dcbf9d251bc71e9da1f94097f
Donovan Adams <donovan.adams@students.makeschool.com>
2018-01-18 09:52:28 -0800

    back end

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,21 +45,20 @@ app.get('/login', (req, res, next)=> {
     });
   });
 
-// SIGN UP POST- handles the user authentication and JWTs
-app.post('/signup', (req, res) => {
-// Create User and JWT
+
+// SIGN UP POST
+  app.post('/sign-up', (req, res) => {
+    // Create User and JWT
     const user = new User(req.body);
 
     user.save().then((user) => {
-      const token = jwt.sign({ _id: user._id, loggedin: "loggedin" }, process.env.SECRET, {
-        expiresIn: "60 days"
-      });
-      res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
-      res.redirect('/');
+      var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
+     res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+     res.redirect('/');
     }).catch((err) => {
       console.log(err.message);
       return res.status(400).send({ err: err });
     });
-    });
+  });
 
 }
